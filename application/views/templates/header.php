93d703f314150350b4508a5fd0cd19a67715d11b
Alison Golden <alisong779@yahoo.com>
2019-12-20 14:51:22 -0500

    First Commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <title>CiBlog</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  </head>
+  <body>
+    
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #450f3f" >
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="/ciBlog">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/ciBlog/index.php/about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/ciBlog/index.php/posts">Blog</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
